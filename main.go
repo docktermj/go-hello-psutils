12a70096b16edda101a1962c1fb5dd668321ca2e
Michael Dockter <michael@dockter.com>
2017-08-01 15:26:57 -0400

    demoCpu() round 2

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/shirou/gopsutil/cpu"
 )
 
@@ -13,20 +15,22 @@ var buildIteration string = "0"
 
 func demoCpu() {
 
-	formatString := "infoStats[%d]: CPU: %9s Guest: %3.1f GuestNice: %3.1f Idle: %7.1f Iowait: %6.1f Irq: %3.1f Nice: %4.1f Softirq: %3.1f Steal: %3.1f Stolen: %3.1f System: %5.1f Total: %7.1f User: %6.1f\n"
-
 	fmt.Printf("\n---------- %s ------------------------------\n\n", "demoCpu()")
 
 	// Number of CPUs.
 
 	cpuCount, _ := cpu.Counts(true)
-	fmt.Printf("Number of CPUs: %d\n", cpuCount)
+	fmt.Printf("cpu.Counts(true): %d\n", cpuCount)
+	cpuCount, _ = cpu.Counts(false)
+	fmt.Printf("cpu.Counts(false): %d\n\n", cpuCount)
 
 	// Per CPU statistics.
 
+	timesFormatString := "timeStats[%d]: CPU: %-9s Guest: %3.1f GuestNice: %3.1f Idle: %7.1f Iowait: %6.1f Irq: %3.1f Nice: %4.1f Softirq: %3.1f Steal: %3.1f Stolen: %3.1f System: %5.1f Total: %8.1f User: %6.1f\n"
+
 	timeStats, _ := cpu.Times(true)
 	for cpuNum := range timeStats {
-		fmt.Printf(formatString,
+		fmt.Printf(timesFormatString,
 			cpuNum,
 			timeStats[cpuNum].CPU,
 			timeStats[cpuNum].Guest,
@@ -50,7 +54,7 @@ func demoCpu() {
 
 	timeStats, _ = cpu.Times(false)
 	for cpuNum := range timeStats {
-		fmt.Printf(formatString,
+		fmt.Printf(timesFormatString,
 			cpuNum,
 			timeStats[cpuNum].CPU,
 			timeStats[cpuNum].Guest,
@@ -67,6 +71,56 @@ func demoCpu() {
 			timeStats[cpuNum].User,
 		)
 	}
+
+	// cpu.Info()
+
+	fmt.Printf("\nCPU Info:\n")
+
+	infoFormatString := "infoStats[%d]: \n\tCPU: %d \n\tCacheSize: %d \n\tCoreID: %s \n\tCores: %d \n\tFamily: %s \n\tFlags: %v \n\tMhz: %4.1f \n\tMicrocode: %s \n\tModel: %s \n\tModelName: %s \n\tPhysicalID: %s \n\tStepping: %d \n\tVendorID: %s\n"
+
+	infoStats, _ := cpu.Info()
+	for cpuNum := range timeStats {
+		fmt.Printf(infoFormatString,
+			cpuNum,
+			infoStats[cpuNum].CPU,
+			infoStats[cpuNum].CacheSize,
+			infoStats[cpuNum].CoreID,
+			infoStats[cpuNum].Cores,
+			infoStats[cpuNum].Family,
+			infoStats[cpuNum].Flags,
+			infoStats[cpuNum].Mhz,
+			infoStats[cpuNum].Microcode,
+			infoStats[cpuNum].Model,
+			infoStats[cpuNum].ModelName,
+			infoStats[cpuNum].PhysicalID,
+			infoStats[cpuNum].Stepping,
+			infoStats[cpuNum].VendorID,
+		)
+	}
+
+	// cpu.Percent()
+
+	percentFormatString := "percent[%d]: %f\n"
+	interval := time.Second
+
+	fmt.Printf("\nPercents(%d, true):\n", interval)
+	percents, _ := cpu.Percent(interval, true)
+	for cpuNum := range timeStats {
+		fmt.Printf(percentFormatString,
+			cpuNum,
+			percents[cpuNum],
+		)
+	}
+
+	fmt.Printf("\nPercents(%d, false):\n", interval)
+	percents, _ = cpu.Percent(interval, false)
+	for cpuNum := range timeStats {
+		fmt.Printf(percentFormatString,
+			cpuNum,
+			percents[cpuNum],
+		)
+	}
+
 }
 
 func main() {
